eb8ce7ce343089e6ff50a98fc6fc4e4fd50fcc32
saltyaom <saltyaom@gmail.com>
2025-09-04 06:52:44 +0700

    :tada: feat: init

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import { Elysia, redirect } from 'elysia'
 import { openapi } from '@elysiajs/openapi'
 import { fromTypes } from '@elysiajs/openapi/gen'
 
-// import User from './modules/user'
+import { User } from './modules/user'
 
 export default new Elysia()
 	.use(
@@ -10,5 +10,5 @@ export default new Elysia()
 			references: fromTypes('api/index.ts')
 		})
 	)
-	// .use(User)
+	.use(User)
 	.compile()
